1a72ebb7b1c126a8201a202cbd64722456db24f9
Jeevan <jeevan15070@gmail.com>
2025-09-23 15:45:04 +0530

    allow all origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,10 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:8080",
-        "http://localhost:8081"
-        ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
